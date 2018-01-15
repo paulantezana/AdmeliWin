22fe37d2a6cb9405756ad2338a6fc938a143a632
DESKTOP-VU52K9S\paulantezana <yoel.antezana@gmail.com>
2018-01-15 08:15:52 -0500

    new updates

--- Admeli/UCIniciar.Designer.cs
+++ Admeli/UCIniciar.Designer.cs
@@ -29,27 +29,42 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
             this.btnContinuar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.cbxPuntosVenta = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.cbxAlmacenes = new System.Windows.Forms.ComboBox();
+            this.cbxPuntosVenta = new System.Windows.Forms.ComboBox();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
-            this.panelContent = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.panelContent.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // panelContent
             // 
-            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.panel1.Controls.Add(this.panelContent);
-            this.panel1.Location = new System.Drawing.Point(77, 79);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(752, 266);
-            this.panel1.TabIndex = 0;
+            this.panelContent.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panelContent.BackColor = System.Drawing.Color.White;
+            this.panelContent.Controls.Add(this.label2);
+            this.panelContent.Controls.Add(this.btnContinuar);
+            this.panelContent.Controls.Add(this.label1);
+            this.panelContent.Controls.Add(this.cbxAlmacenes);
+            this.panelContent.Controls.Add(this.cbxPuntosVenta);
+            this.panelContent.Location = new System.Drawing.Point(87, 66);
+            this.panelContent.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(507, 184);
+            this.panelContent.TabIndex = 6;
+            this.panelContent.Paint += new System.Windows.Forms.PaintEventHandler(this.panelContent_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 47);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Seleccionar Almacén :";
             // 
             // btnContinuar
             // 
@@ -62,10 +77,10 @@
             this.btnContinuar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnContinuar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(183)))));
             this.btnContinuar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnContinuar.Location = new System.Drawing.Point(265, 145);
+            this.btnContinuar.Location = new System.Drawing.Point(199, 118);
             this.btnContinuar.Margin = new System.Windows.Forms.Padding(0);
             this.btnContinuar.Name = "btnContinuar";
-            this.btnContinuar.Size = new System.Drawing.Size(147, 40);
+            this.btnContinuar.Size = new System.Drawing.Size(110, 32);
             this.btnContinuar.TabIndex = 5;
             this.btnContinuar.Text = "Continuar";
             this.btnContinuar.UseVisualStyleBackColor = false;
@@ -74,77 +89,57 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(359, 58);
+            this.label1.Location = new System.Drawing.Point(269, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(192, 17);
+            this.label1.Size = new System.Drawing.Size(146, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Seleccionar Punto de Venta :";
-            // 
-            // cbxPuntosVenta
-            // 
-            this.cbxPuntosVenta.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbxPuntosVenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbxPuntosVenta.FormattingEnabled = true;
-            this.cbxPuntosVenta.Location = new System.Drawing.Point(361, 84);
-            this.cbxPuntosVenta.Name = "cbxPuntosVenta";
-            this.cbxPuntosVenta.Size = new System.Drawing.Size(234, 26);
-            this.cbxPuntosVenta.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(69, 58);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(148, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Seleccionar Almacén :";
             // 
             // cbxAlmacenes
             // 
             this.cbxAlmacenes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbxAlmacenes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxAlmacenes.FormattingEnabled = true;
-            this.cbxAlmacenes.Location = new System.Drawing.Point(70, 84);
+            this.cbxAlmacenes.Location = new System.Drawing.Point(52, 68);
+            this.cbxAlmacenes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbxAlmacenes.Name = "cbxAlmacenes";
-            this.cbxAlmacenes.Size = new System.Drawing.Size(234, 26);
+            this.cbxAlmacenes.Size = new System.Drawing.Size(176, 23);
             this.cbxAlmacenes.TabIndex = 0;
+            // 
+            // cbxPuntosVenta
+            // 
+            this.cbxPuntosVenta.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxPuntosVenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxPuntosVenta.FormattingEnabled = true;
+            this.cbxPuntosVenta.Location = new System.Drawing.Point(271, 68);
+            this.cbxPuntosVenta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbxPuntosVenta.Name = "cbxPuntosVenta";
+            this.cbxPuntosVenta.Size = new System.Drawing.Size(176, 23);
+            this.cbxPuntosVenta.TabIndex = 1;
             // 
             // errorProvider1
             // 
             this.errorProvider1.ContainerControl = this;
             // 
-            // panelContent
-            // 
-            this.panelContent.Controls.Add(this.label2);
-            this.panelContent.Controls.Add(this.btnContinuar);
-            this.panelContent.Controls.Add(this.label1);
-            this.panelContent.Controls.Add(this.cbxAlmacenes);
-            this.panelContent.Controls.Add(this.cbxPuntosVenta);
-            this.panelContent.Location = new System.Drawing.Point(38, 20);
-            this.panelContent.Name = "panelContent";
-            this.panelContent.Size = new System.Drawing.Size(676, 227);
-            this.panelContent.TabIndex = 6;
-            this.panelContent.Paint += new System.Windows.Forms.PaintEventHandler(this.panelContent_Paint);
-            // 
             // UCIniciar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.panel1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.Controls.Add(this.panelContent);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "UCIniciar";
-            this.Size = new System.Drawing.Size(906, 389);
+            this.Size = new System.Drawing.Size(680, 316);
             this.Load += new System.EventHandler(this.UCIniciar_Load);
-            this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.panelContent.ResumeLayout(false);
             this.panelContent.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ComboBox cbxPuntosVenta;
         private System.Windows.Forms.ComboBox cbxAlmacenes;
         private System.Windows.Forms.Label label1;
