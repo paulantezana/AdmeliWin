1a7b3214d62929e76b3b1cff88b004c144e8b344
yoel <yoel.antezana@gmail.com>
2018-01-15 19:44:55 -0500

    new updates

--- Admeli/Configuracion/UCGrupoClientes.cs
+++ Admeli/Configuracion/UCGrupoClientes.cs
@@ -7,22 +7,178 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Modelo;
+using Admeli.Componentes;
+using Entidad;
 
 namespace Admeli.Configuracion
 {
     public partial class UCGrupoClientes : UserControl
     {
         private FormPrincipal formPrincipal;
+        private Paginacion paginacion;
+        private GrupoClienteModel grupoClienteModel = new GrupoClienteModel();
 
         public UCGrupoClientes()
         {
             InitializeComponent();
+
+            lblSpeedPages.Text = ConfigModel.configuracionGeneral.itemPorPagina.ToString();     // carganto los items por página
+            paginacion = new Paginacion(Convert.ToInt32(lblCurrentPage.Text), Convert.ToInt32(lblSpeedPages.Text));
         }
 
         public UCGrupoClientes(FormPrincipal formPrincipal)
         {
             InitializeComponent();
             this.formPrincipal = formPrincipal;
+
+            lblSpeedPages.Text = ConfigModel.configuracionGeneral.itemPorPagina.ToString();     // carganto los items por página
+            paginacion = new Paginacion(Convert.ToInt32(lblCurrentPage.Text), Convert.ToInt32(lblSpeedPages.Text));
         }
+
+        private void UCGrupoClientes_Load(object sender, EventArgs e)
+        {
+            cargarComponentes();
+            cargarRegistros();
+        }
+
+        #region =========================== Decoration ===========================
+        private void decorationDataGridView()
+        {
+            /*
+            for (int i = 0; i < dataGridView.Rows.Count; i++)
+            {
+                var estado = dataGridView.Rows[i].Cells.get.Value.ToString();
+                dataGridView.Rows[i].DefaultCellStyle.BackColor = Color.DeepPink;
+            }*/
+        }
+        #endregion
+
+        #region ======================= Loads =======================
+        private async void cargarComponentes()
+        {
+            // Cargando el combobox de personales
+            // loadState(true);
+            // Estado cargar en falso
+            // loadState(false);
+        }
+
+        private async void cargarRegistros()
+        {
+            loadState(true);
+            try
+            {
+                RootObject<GrupoCliente> grupoClientes = await grupoClienteModel.gclientes(paginacion.currentPage, paginacion.speed);
+
+                // actualizando datos de páginacón
+                paginacion.itemsCount = grupoClientes.nro_registros;
+                paginacion.reload();
+
+                // Ingresando
+                grupoClienteBindingSource.DataSource = grupoClientes.datos;
+                dataGridView.Refresh();
+                mostrarPaginado();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Listar", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            finally
+            {
+                loadState(false);
+            }
+        }
+        #endregion
+
+        #region =========================== Estados ===========================
+        private void loadState(bool state)
+        {
+            formPrincipal.appLoadState(state);
+            toolStripNavigation.Enabled = !state;
+            toolStripCrud.Enabled = !state;
+            dataGridView.Enabled = !state;
+        }
+        #endregion
+
+        #region ===================== Eventos Páginación =====================
+        private void mostrarPaginado()
+        {
+            // Cargando el combobox
+            lblCurrentPage.Items.Clear();
+            for (int i = 1; i <= paginacion.pageCount; i++)
+            {
+                lblCurrentPage.Items.AddRange(new object[] { i.ToString() });
+            }
+            if (paginacion.pageCount != 0) lblCurrentPage.SelectedIndex = paginacion.currentPage - 1;
+
+            // Paginados
+            lblPageAllItems.Text = paginacion.itemsCount.ToString();
+            lblPageCount.Text = paginacion.pageCount.ToString();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (lblCurrentPage.Text != "1")
+            {
+                paginacion.previousPage();
+                cargarRegistros();
+            }
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            if (lblCurrentPage.Text != "1")
+            {
+                paginacion.firstPage();
+                cargarRegistros();
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSpeedPages_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                paginacion.speed = Convert.ToInt32(lblSpeedPages.Text);
+                paginacion.currentPage = 1;
+                cargarRegistros();
+            }
+        }
+
+        private void lblCurrentPage_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                paginacion.reloadPage(Convert.ToInt32(lblCurrentPage.Text));
+                cargarRegistros();
+            }
+        }
+        #endregion
+
+        #region ==================== CRUD ====================
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            cargarRegistros();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            cargarRegistros();
+        }
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            /*FormComprarNuevo comprarNuevo = new FormComprarNuevo();
+            comprarNuevo.ShowDialog();*/
+        }
+        #endregion
     }
 }
---
+++ Modelo/GrupoClienteModel.cs
@@ -0,0 +1,44 @@
+﻿using Entidad;
+using Modelo.Recursos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class GrupoClienteModel
+    {
+        private WebService webService = new WebService();
+
+        public void guardar()
+        {
+
+        }
+
+        public void modificar()
+        {
+
+        }
+
+        public void eliminar()
+        {
+
+        }
+
+        public async Task<RootObject<GrupoCliente>> gclientes(int page, int items)
+        {
+            try
+            {
+                // www.lineatienda.com/services.php/gclientes/estado/1/100
+                RootObject<GrupoCliente> grupoClientes = await webService.GETRoot<GrupoCliente>("gclientes", String.Format("estado/{0}/{1}", page, items));
+                return grupoClientes;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
