7cfaf50722e75e173d7d892b77e5a355e8712c56
paulantezana <yoel.antezana@gmail.com>
2018-02-15 17:52:25 -0500

    new updates

--- Admeli/Form1.Designer.cs
+++ Admeli/Form1.Designer.cs
@@ -28,29 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.bunifuCards1 = new Bunifu.Framework.UI.BunifuCards();
             this.SuspendLayout();
-            // 
-            // bunifuCards1
-            // 
-            this.bunifuCards1.BackColor = System.Drawing.Color.White;
-            this.bunifuCards1.BorderRadius = 5;
-            this.bunifuCards1.BottomSahddow = true;
-            this.bunifuCards1.color = System.Drawing.Color.Tomato;
-            this.bunifuCards1.LeftSahddow = false;
-            this.bunifuCards1.Location = new System.Drawing.Point(38, 25);
-            this.bunifuCards1.Name = "bunifuCards1";
-            this.bunifuCards1.RightSahddow = true;
-            this.bunifuCards1.ShadowDepth = 20;
-            this.bunifuCards1.Size = new System.Drawing.Size(197, 213);
-            this.bunifuCards1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.bunifuCards1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -58,7 +42,5 @@
         }
 
         #endregion
-
-        private Bunifu.Framework.UI.BunifuCards bunifuCards1;
     }
 }
