f700f56b3b961003e99b8cc110ac2c5026e7fc7c
cyper <093157@unsaac.edu.pe>
2018-02-24 11:59:03 -0500

    new cyper update

--- Admeli/Configuracion/Modificar/FormDiseñoComprobantes.cs
+++ Admeli/Configuracion/Modificar/FormDiseñoComprobantes.cs
@@ -14,19 +14,27 @@ using Entidad;
 using System.Globalization;
 using Bunifu.Framework.UI;
 using Modelo;
+using System.Threading;
+
 namespace Admeli.Configuracion.Modificar
 {
     public partial class FormDiseñoComprobantes : Form
     {
-        
+
+
+        List<ResizeableControl> listaElemtos;
+        ResizeableControl rc;
         DiseñoDocumento diseño;
         List<FormatoDocumento>  formato;
         String json;
-
+        bool redi = false;
         Label aux;
         PictureBox cuadro;
+
         List<vineta> vinetas = new List<vineta>();
-        List<Label> listLabel = new List<Label>();
+
+        List<vineta> listLabel = new List<vineta>();// label adiciolanes "texto"
+
         List<vineta> listGridField = new List<vineta>();
         vineta detalleBtn;
         DataGridView detalle; 
@@ -41,6 +49,9 @@ namespace Admeli.Configuracion.Modificar
         {
 
             InitializeComponent();
+
+            panel4.AutoSize = false;
+            listaElemtos = new List<ResizeableControl>();
             detalle = new DataGridView();
            
             detalle.RowHeadersVisible = false;
@@ -174,6 +185,13 @@ namespace Admeli.Configuracion.Modificar
 
         private void crearListGrid()
         {
+            //para el texto
+            vineta aux1 = new vineta();
+            aux1.label.Text = "";
+            aux1.nombre = "";
+            listLabel.Add(aux1);
+            //
+
             vineta aux = new vineta();
             aux.label.Text = "codigoProducto";
             aux.nombre = "codigoProducto";
@@ -228,25 +246,32 @@ namespace Admeli.Configuracion.Modificar
                 {
                     case "Label":
                         vineta aux=buscarVineta(doc.value,vinetas);
+
+                        if (aux == null)
+                         {
+                            aux = new vineta();
+                         }
+                          
+
                         aux.label.Text = doc.value;
                         aux.usado = 3;
                         aux.nombre = doc.value;
-                        aux.label.AutoSize = true;
+                        aux.label.AutoSize = false;
                         // string colorcode = form.color;
                         //int argb = Int32.Parse(colorcode.Replace("#", ""), NumberStyles.HexNumber);
                         aux.label.BackColor = System.Drawing.Color.Fuchsia;
                         aux.label.Location = new System.Drawing.Point(doc.x, doc.y);
                         aux.label.Size = new System.Drawing.Size((int)doc.w, (int)doc.h);
-                        aux.label.MouseDown += new System.Windows.Forms.MouseEventHandler(this.vineta_MouseDown);
-                        aux.label.MouseMove += new System.Windows.Forms.MouseEventHandler(this.vineta_MouseMove);
-                        aux.label.MouseUp += new System.Windows.Forms.MouseEventHandler(this.vineta_MouseUp);
-                     //   aux.label.
                        
+                        //   aux.label.
+
 
 
                         aux.label.TabIndex = 5;
-                        aux.label.AutoSize = true;
+                       
                         this.panel4.Controls.Add(aux.label);
+                        ResizeableControl rc = new ResizeableControl(aux.label);
+                        listaElemtos.Add(rc);
 
                         
                      break;
@@ -255,11 +280,12 @@ namespace Admeli.Configuracion.Modificar
                         detalle.Size = new Size((int)doc.w, (int)doc.h);
                         detalle.Name = doc.formato;
                         detalleBtn.usado = 3;
-                        detalle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dataGridView_MouseDown);
-                        detalle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dataGridView_MouseMove);
-                        detalle.MouseUp += new System.Windows.Forms.MouseEventHandler(this.dataGridView_MouseUp);
+                        
+
 
                         this.panel4.Controls.Add(detalle);
+                         rc = new ResizeableControl(detalle);
+                        listaElemtos.Add(rc);
                         break;
                     case "ListGridField":
                         vineta aux1 = buscarVineta(doc.formato, listGridField);
@@ -270,18 +296,16 @@ namespace Admeli.Configuracion.Modificar
 
                         // \Recursos\Darck
 
-                        string imagen = "E:\\code\\AdmeliWin\\Recursos\\Darck\\box_gray_icon.png";
-                        cuadro.Image = Image.FromFile(imagen);
+                        //string imagen = "E:\\code\\AdmeliWin\\Recursos\\Darck\\box_gray_icon.png";
+                      //  cuadro.Image = Image.FromFile(imagen);
                         cuadro.Location = new Point(doc.x, doc.y);
                         cuadro.BackColor = Color.Green;
 
                        
                         cuadro.Size = new Size((int)doc.w, (int)doc.h);
-                        cuadro.MouseDown += new System.Windows.Forms.MouseEventHandler(this.cuadro_MouseDown);
-                        cuadro.MouseMove += new System.Windows.Forms.MouseEventHandler(this.cuadro_MouseMove);
-                        cuadro.MouseUp += new System.Windows.Forms.MouseEventHandler(this.cuadro_MouseUp);
                         this.panel4.Controls.Add(cuadro);
-                      
+                        rc = new ResizeableControl(cuadro);
+                        listaElemtos.Add(rc);
                         break;
                    
 
@@ -314,6 +338,27 @@ namespace Admeli.Configuracion.Modificar
         {
             
             int Y = 10;
+            Button bunifuThinButton = new Button();
+
+            bunifuThinButton.BackColor = System.Drawing.Color.LightGray;
+            bunifuThinButton.Text = "texto";
+            bunifuThinButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            bunifuThinButton.Font = new System.Drawing.Font("Century Gothic", 7.5f, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            bunifuThinButton.ForeColor = System.Drawing.Color.DarkBlue;
+            bunifuThinButton.FlatAppearance.BorderColor = Color.SlateGray;
+            bunifuThinButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            bunifuThinButton.Location = new System.Drawing.Point(20, Y);
+            bunifuThinButton.Margin = new System.Windows.Forms.Padding(5);
+            bunifuThinButton.Size = new System.Drawing.Size(160, 30);
+            bunifuThinButton.TabIndex = 5;
+            bunifuThinButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+
+
+            
+            this.panel1.Controls.Add(bunifuThinButton);
+
+            Y += 43;
             foreach (vineta v1 in listGridField)
             {
                 
@@ -423,7 +468,17 @@ namespace Admeli.Configuracion.Modificar
                     aux=buscarVineta(c1.Text, listGridField);
                     if (aux == null)
                     {
-                        aux = detalleBtn;
+                        aux= buscarVineta("", listLabel);
+
+                        if(aux==null)
+
+                            aux = detalleBtn;
+                        else
+                        {
+                            
+                            aux.usado = 1;
+                        }
+                           
                     }
                 }
                 if (aux.usado == 1)
@@ -460,36 +515,50 @@ namespace Admeli.Configuracion.Modificar
             
         }
 
+        private void vineta_MouseHover(object sender, EventArgs e)
+        {
+            
+            Label c1 = sender as Label;
+            vineta aux = buscarVineta(c1.Text, vinetas);
+            panel1.Cursor = Cursors.SizeAll;
+            redi=true;
+            aux.w = c1.Width;
+            aux.h = c1.Height;
+            aux.label.BackColor = Color.Aqua;
+            aux.label.FlatStyle = FlatStyle.Flat;
+        }
+        /*obsoleto
         private void vineta_MouseDown(object sender, MouseEventArgs e)
         {
-
-           
-
-        
+            
             Label label = sender as Label;
 
             
             vineta aux=buscarVineta(label.Text,vinetas);
+            
             aux.posicionX = e.X;
             aux.posicionY = e.Y;
             int x = label.Width;
             int y = label.Height;
-            bool uno = esta1(0, x, 0,y,e.X, e.Y);
-
-            if(uno)
+            //bool uno = esta1(0, x, 0,y,e.X, e.Y);
+            if (!redi)
             {
-                label.Cursor = Cursors.Cross;
+                aux.mover = true;
+
+
             }
-            aux.mover = true;
-
-
-            
-    
+            else
+            {
+                aux.redimensionar = true;
+                Thread.Sleep(10000);
+            }
+                
+           
         }
         private void vineta_MouseMove(object sender, MouseEventArgs e)
         {
 
-
+            
             Label label = sender as Label;
             vineta aux = buscarVineta(label.Text, vinetas);
             if (aux.mover)
@@ -499,17 +568,61 @@ namespace Admeli.Configuracion.Modificar
                 this.Refresh();
 
             }
+            else
+                if (aux.redimensionar)
+                 {
+                        RedimensionaLados(aux.label, e.X, e.Y);
+                       // aux.label.Location = new Point(aux.label.Location.X + e.X - aux.posicionX, aux.label.Location.Y + e.Y - aux.posicionY);
+
+                        //aux.label.Size = new Size(aux.label.Width + aux.posicionX-e.X, aux.label.Height +aux.posicionX -e.Y );
+                       
+                   }
         }
 
-    
-
-        private void vineta_MouseUp(object sender, EventArgs e)
+        public void RedimensionaLados(Label label, int X, int Y)
         {
+            // arriba 
+            int V1 = Label.MousePosition.X-(panel3.Location.X+panel4.Location.X);
+            int V2 = Label.MousePosition.Y-(panel3.Location.Y + panel4.Location.Y);
+            // int vX=
+
+            if (Y < 0)
+            {
+                Y= Math.Abs(Y);
+              //  Thread.Sleep(10000);
+            }
+                
+             
+            else
+                Y = -Y;
+            
+                
+            int nuevox = label.Width;
+            int nuevoy = label.Height  + Y;
+            label.Size = new Size(nuevox,nuevoy);
+            label.Location = new Point(label.Location.X,Y);
+           
+            // abajo
+            //derecha
+            //izquierda
+        }
+
+        private void vineta_MouseUp(object sender, MouseEventArgs e)
+        {
+           
+               
             Label label = sender as Label;
             vineta aux = buscarVineta(label.Text, vinetas);
-             label.Cursor = Cursors.Default;
             
+             label.Cursor = Cursors.Default;
+            if (redi && aux.redimensionar)
+            {   redi = false;
+                //label.Location = new Point(label.Location.X, e.Y);
+
+            }
             aux.mover = false;
+            aux.redimensionar = false;
+           
         }
 
         private void cuadro_MouseDown(object sender , MouseEventArgs e)
@@ -536,7 +649,7 @@ namespace Admeli.Configuracion.Modificar
 
         }
 
-
+        */
 
         private void labels_MouseDown(object sender, MouseEventArgs e)
         {
@@ -570,21 +683,22 @@ namespace Admeli.Configuracion.Modificar
             Button c1 = sender as Button;
             vineta aux = buscarVineta(c1.Text, vinetas);
             
-            aux.mover = true;
+           
            
             aux.label.BackColor = Color.Black;
             aux.label.Location = new Point(c1.Location.X - (panel3.Location.X + panel4.Location.X), c1.Location.Y - panel4.Location.Y);
 
-            aux.label.MouseDown += new System.Windows.Forms.MouseEventHandler(this.vineta_MouseDown);
-            aux.label.MouseMove += new System.Windows.Forms.MouseEventHandler(this.vineta_MouseMove);
-            aux.label.MouseUp += new System.Windows.Forms.MouseEventHandler(this.vineta_MouseUp);
-
+    
+            aux.label.AutoSize = false;
 
             panel4.Controls.Add(aux.label);
+          
+
             aux.usado = 3;// esta en el panel1 ahora
             panel1.Controls.Remove(c1); // el boton se elimina de panel1
             c1.Location = new Point(aux.inicialX, aux.inicialY);
-
+            rc = new ResizeableControl(aux.label);
+            listaElemtos.Add(rc);
             aux.mover = false;
             reload();
         }
@@ -593,7 +707,8 @@ namespace Admeli.Configuracion.Modificar
         {
             Button c1 = sender as Button;
             vineta aux = buscarVineta(c1.Text, listGridField);
-           
+            if (aux == null)
+                aux = buscarVineta("",listLabel);
             aux.posicionX = e.X;
             aux.posicionY = e.Y;
 
@@ -608,6 +723,8 @@ namespace Admeli.Configuracion.Modificar
         {
             Button c1 = sender as Button;
             vineta aux = buscarVineta(c1.Text, listGridField);
+            if( aux==null)
+                aux = buscarVineta("", listLabel);
             if (aux.mover)
             {
                 c1.Location = new Point(c1.Location.X + e.X - posicionX, c1.Location.Y + e.Y - posicionY);
@@ -621,22 +738,52 @@ namespace Admeli.Configuracion.Modificar
         {
             Button c1 = sender as Button;
             vineta aux = buscarVineta(c1.Text, listGridField);
-            bool uno = esta1(detalle.Location.X, detalle.Width, detalle.Location.Y, detalle.Height, c1.Location.X - (panel3.Location.X + panel4.Location.X), c1.Location.Y - panel4.Location.Y);
-            
+
+            if (aux == null)
+            {
+                String name = Microsoft.VisualBasic.Interaction.InputBox(null, "mensaje", "escriba el nombre del nuevo campo");
+                aux = buscarVineta("", listLabel);
+                if (name != "")
+                {
+                    aux.label.BackColor = Color.Black;
+                    aux.label.Location = new Point(c1.Location.X - (panel3.Location.X + panel4.Location.X), c1.Location.Y - panel4.Location.Y);
+
+                    aux.nombre = name;
+                    aux.label.AutoSize = false;
+                    c1.Location = new Point(aux.inicialX, aux.inicialY);
+                    panel4.Controls.Add(aux.label);
+                    vineta aux1 = new vineta();
+                    aux1.label.Text = "";
+                    aux1.nombre = "";
+                    listLabel.Add(aux1);
+                    aux.mover = false;
+                    rc = new ResizeableControl(aux.label);
+                    listaElemtos.Add(rc);
+                    reload();
+
+                }
+
+
+
+            }
+            else { 
+                bool uno = esta1(detalle.Location.X, detalle.Width, detalle.Location.Y, detalle.Height, c1.Location.X - (panel3.Location.X + panel4.Location.X), c1.Location.Y - panel4.Location.Y);
+
                 if (esta(detalle.Location.X, detalle.Width, detalle.Location.Y, detalle.Height, c1.Location.X - (panel3.Location.X + panel4.Location.X), c1.Location.Y - panel4.Location.Y))
-                { if (!buscarColumna(c1.Text))
+                {
+                    if (!buscarColumna(c1.Text))
                     {
-                    detalle.Columns.Add(c1.Text, c1.Text);
-                    aux.usado = 3;
+                        detalle.Columns.Add(c1.Text, c1.Text);
+                        aux.usado = 3;
                     }
                 }
-                if(uno)
+                if (uno)
                     detalle.Cursor = Cursors.Default;
-            c1.Location = new Point(aux.inicialX,aux.inicialY);
-   
-            aux.mover = false;
-            reload();
+                c1.Location = new Point(aux.inicialX, aux.inicialY);
 
+                aux.mover = false;
+                reload();
+            }
         }
         
         private void dataGrid_MouseDown(object sender, MouseEventArgs e)
@@ -667,15 +814,13 @@ namespace Admeli.Configuracion.Modificar
             Button c1 = sender as Button;
             detalle.Location = new Point(c1.Location.X - (panel3.Location.X + panel4.Location.X), c1.Location.Y - panel4.Location.Y);
 
-            detalle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dataGridView_MouseDown);
-            detalle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dataGridView_MouseMove);
-            detalle.MouseUp += new System.Windows.Forms.MouseEventHandler(this.dataGridView_MouseUp);
-
             panel4.Controls.Add(detalle);
             detalleBtn.usado = 3;
             panel1.Controls.Remove(c1);
             //c1.Location = new Point(detalleBtn.inicialX, detalleBtn.inicialY);
             detalleBtn.mover = false;
+            rc = new ResizeableControl(detalle);
+            listaElemtos.Add(rc);
             //actualizar estado de 
             reload();
         }
@@ -778,6 +923,26 @@ namespace Admeli.Configuracion.Modificar
             }
 
             FormatoDocumento nuevo2 = new FormatoDocumento();
+            foreach (vineta v1 in listLabel)
+            {
+                if (v1.nombre != "")
+                {
+                    FormatoDocumento nuevoFormato = new FormatoDocumento();
+                    nuevoFormato.color = v1.label.BackColor.ToString();
+                    nuevoFormato.formato = v1.label.Text;
+                    nuevoFormato.tipo = "Label";
+                    nuevoFormato.h = v1.label.Height;
+                    nuevoFormato.w = v1.label.Width;
+                    nuevoFormato.x = v1.label.Location.X;
+                    nuevoFormato.y = v1.label.Location.Y;
+                    nuevoFormato.value = v1.nombre;
+                    listFormato.Add(nuevoFormato);
+
+                }
+
+            }
+            // label adicionales
+
 
             /*
             panel3.Controls.Find()
@@ -790,8 +955,22 @@ namespace Admeli.Configuracion.Modificar
             nuevo1.y = cuadro.Location.Y;
             nuevo1.value = "Imagen";
             listFormato.Add(nuevo1);*/
+            //pagina
 
-            nuevo.formatoDocumento = JsonConvert.SerializeObject(listFormato);
+            FormatoDocumento nuevoFormato1 = new FormatoDocumento();
+            nuevoFormato1.color = panel4.BackColor.ToString();
+            nuevoFormato1.formato = panel4.Text;
+            nuevoFormato1.tipo = "Pagina";
+            nuevoFormato1.h = panel4.Height;
+            nuevoFormato1.w = panel4.Width;
+            nuevoFormato1.x = panel4.Location.X;
+            nuevoFormato1.y = panel4.Location.Y;
+            nuevoFormato1.value = "Pagina";
+            listFormato.Add(nuevoFormato1);
+            
+
+
+                nuevo.formatoDocumento = JsonConvert.SerializeObject(listFormato);
 
             modificarformato(nuevo);
             redimensionar(redi);
